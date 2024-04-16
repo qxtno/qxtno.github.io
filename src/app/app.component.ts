f5a4da557a744eee23171a761721078346107099
kacper.sledz <casper.sledx@gmail.com>
2024-04-16 22:57:38 +0200

    meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'karolsledz';
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private metaService: Meta
+  ) {}
+
+  ngOnInit(): void {
+    this.metaService.updateTag({ name: 'author', content: 'karolsledz' });
+    this.metaService.updateTag({
+      name: 'keywords',
+      content: 'karolsledz,angular,portfolio',
+    });
+    this.metaService.updateTag({ name: 'robots', content: 'index,follow' });
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const rt = this.getChild(this.activatedRoute);
+        rt.data.subscribe((data) => {
+          const description = data['description'];
+          if (description) {
+            this.metaService.updateTag({
+              name: 'description',
+              content: description,
+            });
+          } else {
+            this.metaService.removeTag('name="description"');
+          }
+        });
+      });
+  }
+
+  getChild(activatedRoute: ActivatedRoute): ActivatedRoute {
+    if (activatedRoute.firstChild) {
+      return this.getChild(activatedRoute.firstChild);
+    } else {
+      return activatedRoute;
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,31 @@ import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, title: "karolsledz" },
-    {
-        path: 'admin',
-        loadComponent: () =>
-            import('./admin-panel/admin-panel.component').then(
-                (m) => m.AdminPanelComponent
-            ),
-        title: "Admin"
-    },
-    { path: 'details', component: DetailsComponent, title: "Details" },
-    { path: 'about', component: AboutComponent, title: "About" }
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'karolsledz',
+    data: { description: 'to jest strona główna' },
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./admin-panel/admin-panel.component').then(
+        (m) => m.AdminPanelComponent
+      ),
+    title: 'Admin',
+    data: { description: 'to jest strona admina' },
+  },
+  {
+    path: 'details',
+    component: DetailsComponent,
+    title: 'Details',
+    data: { description: 'to jest strona details' },
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'About',
+    data: { description: 'to jest strona about' },
+  },
 ];
